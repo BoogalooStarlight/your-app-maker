5341823c301c77f785a12d9efcd4332aad2e24ff
BoogalooStarlight <boogalooIA@proton.me>
2026-02-25 19:40:01 +0100

    Add temporary Modules and Trophies pages to unblock build

---
+++ src/components/AppNavigation.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "@/components/NavLink";
+
+export const AppNavigation = () => {
+  return (
+    <nav className="flex items-center gap-4">
+      <NavLink to="/modules" className="text-white/70" activeClassName="text-white">
+        Modules
+      </NavLink>
+      <NavLink to="/trophees" className="text-white/70" activeClassName="text-white">
+        Trophées
+      </NavLink>
+    </nav>
+  );
+};
